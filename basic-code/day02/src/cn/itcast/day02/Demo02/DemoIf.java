5f01acb8bdce20e0b443cabfbeaa558a11e25196
zsd-oo <zsd-oo@163.com>
2021-11-05 14:34:43 +0800

    1105-2

--- basic-code/day02/src/cn/itcast/day02/Demo02/DemoIf.java
+++ basic-code/day02/src/cn/itcast/day02/Demo02/DemoIf.java
@@ -4,7 +4,7 @@ public class DemoIf {
     public static void main(String[] args) {
         System.out.println("今天天气不错，正在压马路……突然发现一个快乐的地方：网吧");
         int age=19;
-        if (age >= 18){
+        if (age = 18){
             System.out.println("进入网吧，开始high!");
             System.out.println("遇到一群猪队友，开始骂街");
             System.out.println("感觉不爽，结账走人。");
@@ -12,5 +12,6 @@ public class DemoIf {
         System.out.println("回家吃饭");
         System.out.println("1105-1");
         System.out.println("1105-2");
+        System.out.println("1105-3");
     }
 }
